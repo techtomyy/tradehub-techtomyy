d3fbe200a277915642a17f530b559aecf7cc64dc
SaqibYounas <muhammadsaqibyounas11>
2025-08-06 17:52:58 +0500

    Final

---
+++ app.js
@@ -0,0 +1,30 @@
+import express, { json } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Middlewares
+app.use(json());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send(`
+    <html>
+      <head><title>Welcome</title></head>
+      <body>
+        <h1>Welcome to Express Server!</h1>
+      </body>
+    </html>
+  `);
+});
+
+
+app.listen(4000,()=>{
+  console.log("server is running");
+})
